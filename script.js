3382b4c654fe57d1022e49478bdd71f45484cded
«viktoriya <viki.lar@yandex.ru>
2022-06-03 21:37:20 +0400

    добавлен функционал по подсчету попыток

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 
 let secretNumber = Math.floor(Math.random() * 100) + 1;
 let num;
+let attempt = 10;
+
 const game = function () {
+  // ввод значения
   function input() {
     num = +prompt("Угадай число от 1 до 100");
     checkNum(num);
   }
+
   //проверка на число
   const checkNum = function (i) {
     if (isNaN(i)) {
@@ -23,23 +27,32 @@ const game = function () {
   };
   input(num);
 
-  const search = function (i) {
-    if (i == secretNumber) {
-      alert("Поздравляю, Вы угадали!!!");
-    } else {
-      if (i > secretNumber) {
-        alert("Загаданное число меньше");
-
+  //проверка на количество попыток
+  if (attempt !== 1) {
+    //проверка на равенство
+    const search = function (i) {
+      attempt--;
+      if (i == secretNumber) {
+        alert("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+        attempt = 10;
         game();
       } else {
-        if (i < secretNumber) {
-          alert("Загаданное число больше");
+        if (i > secretNumber) {
+          alert("Загаданное число меньше, осталось попыток" + " " + attempt);
 
           game();
+        } else {
+          if (i < secretNumber) {
+            alert("Загаданное число больше,осталось попыток" + " " + attempt);
+
+            game();
+          }
         }
       }
-    }
-  };
-  search(num);
+    };
+    search(num);
+  } else {
+    alert("Попытки закончились, хотите сыграть еще?");
+  }
 };
 game();
