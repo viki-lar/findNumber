aa2849c4cfddec531029c6172be4de7f6f12b5b8
«viktoriya <viki.lar@yandex.ru>
2022-06-05 21:43:29 +0400

    устранены замечания по оформлению, изменены условия

--- script.js
+++ script.js
@@ -2,63 +2,51 @@
 
 const game = function () {
   let secretNumber = Math.floor(Math.random() * 100) + 1;
-  let num;
+  let num = "";
   let attempt = 10;
   let begineMessage;
   let answer;
 
-  begineMessage = confirm("Хочешь сыграть в игру 'Угадай число?'");
+  const round = function () {
+    num = prompt("Введи число от 1 до 100");
 
-  if (begineMessage == true) {
-    if (attempt !== 1) {
-      const round = function () {
-        num = prompt("Введи число от 1 до 100");
+    //проверка на отмену
+    if (num == null || num == 0) alert("Игра окончена");
+    else {
+      // проверка на число
+      if (isNaN(num)) {
+        alert("Введи число!");
+        round();
+      } else {
+        num = +num;
 
-        //проверка на отмену
-        if (num == null || num == 0) {
-          alert("Игра окончена");
-          game();
-        } else {
-          // проверка на число
-          if (isNaN(num)) {
-            num = alert("Введи число!");
-            console.log(num);
-            round();
-          } else {
-            num = +num;
+        //проверка на равенство
 
-            //проверка на равенство
-
-            if (num == secretNumber) {
-              answer = confirm(
-                "Поздравляю, Вы угадали!!! Хотели бы сыграть еще?"
-              );
-              if (answer == true) {
-                game();
-              }
-            }
-
-            if (num > secretNumber) {
-              attempt--;
-              alert(
-                "Загаданное число меньше, осталось попыток" + " " + attempt
-              );
-              round();
-            } else {
-              if (num < secretNumber) {
-                attempt--;
-                alert(
-                  "Загаданное число больше,осталось попыток" + " " + attempt
-                );
-                round();
-              }
-            }
+        if (num == secretNumber) {
+          answer = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+          if (answer) {
+            game();
           }
         }
-      };
+
+        if (num > secretNumber) {
+          alert("Загаданное число меньше, осталось попыток" + " " + attempt);
+        } else if (num < secretNumber) {
+          alert("Загаданное число больше,осталось попыток" + " " + attempt);
+        }
+        attempt--;
+        round();
+      }
+    }
+  };
+
+  begineMessage = confirm("Хочешь сыграть в игру 'Угадай число?'");
+
+  if (begineMessage) {
+    if (attempt > 1) {
       round();
     } else {
-      alert("Попытки закончились, хотите сыграть еще?");
+      begineMessage = confirm("Попытки закончились, хотите сыграть еще?");
     }
   }
 };
